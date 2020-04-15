70ec14d2441758a633ad860e8cbef44e056359d0
Jim Pick <jim@protocol.ai>
2020-04-14 21:42:28 -0700

    Update code with some error handling, tokens for websockets, README and package.json fixes

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Lotus JS Client - Web Browser Provider
 
+This package contains an implementation of the Lotus JS Client provider interface
+that connects to a [Lotus](https://github.com/filecoin-project/lotus) JSON-RPC
+API endpoint using WebSockets or HTTP.
+
+See the [filecoin-shipyard/js-lotus-client](https://github.com/filecoin-shipyard/js-lotus-client) for examples that show how to use this provider.
+
 ## License
 
 Dual-licensed under [MIT](https://github.com/filecoin-project/lotus/blob/master/LICENSE-MIT) + [Apache 2.0](https://github.com/filecoin-project/lotus/blob/master/LICENSE-APACHE)
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-export default class BrowserProvider {
+class BrowserProvider {
   constructor (url, options = {}) {
     this.url = url
-    this.httpUrl = url.replace(/^wss:/, 'https:' )
+    this.httpUrl = url.replace(/^wss:/, 'https:')
     this.id = 0
     this.inflight = new Map()
     this.subscriptions = new Map()
     this.token = options.token
+    if (this.token && this.token !== '') {
+      this.url += `?token=${this.token}`
+    }
   }
 
   connect () {
@@ -28,11 +31,7 @@ export default class BrowserProvider {
       id: this.id++,
       ...request
     }
-    if (schemaMethod.perm === 'write') {
-      return this.sendHttp(jsonRpcRequest)
-    } else {
-      return this.sendWs(jsonRpcRequest)
-    }
+    return this.sendWs(jsonRpcRequest)
   }
 
   async sendHttp (jsonRpcRequest) {
@@ -50,7 +49,11 @@ export default class BrowserProvider {
         body: JSON.stringify(jsonRpcRequest)
       })
       // FIXME: Check return code, errors
-      const { result } = await response.json()
+      const { error, result } = await response.json()
+      if (error) {
+        // FIXME: Return error class with error.code
+        throw new Error(error.message)
+      }
       return result
     } catch (e) {
       throw e
@@ -104,14 +107,10 @@ export default class BrowserProvider {
 
   receive (event) {
     try {
-      const {
-        id,
-        result,
-        method,
-        params
-      } = JSON.parse(event.data)
-      // FIXME: JSON-RPC errors?
+      const { id, error, result, method, params } = JSON.parse(event.data)
+      // FIXME: Check return code, errors
       if (method === 'xrpc.ch.val') {
+        // FIXME: Check return code, errors
         const [chanId, data] = params
         const subscriptionCb = this.subscriptions.get(chanId)
         if (subscriptionCb) {
@@ -123,7 +122,13 @@ export default class BrowserProvider {
         const cb = this.inflight.get(id)
         if (cb) {
           this.inflight.delete(id)
+          if (error) {
+            // FIXME: Return error class with error.code
+            return cb(new Error(error.message))
+          }
           cb(null, result)
+        } else {
+          console.warn(`Couldn't find subscription for ${id}`)
         }
       }
     } catch (e) {
@@ -137,3 +142,5 @@ export default class BrowserProvider {
     }
   }
 }
+
+export default BrowserProvider
